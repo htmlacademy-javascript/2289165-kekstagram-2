f776d892322b30d096629ccfcd4f6c72462d1412
Semenets Ekaterina <cortes.lex@gmail.com>
2025-06-16 10:54:11 +0300

    feat: Добавляет фильтрацию, добавление фото

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,6 @@
 import { ALERT_SHOW_TIME } from './data.js';
 
-
 export const isEscapeKey = (evt) => evt.key === 'Escape';
-
 
 const alertTemplate = document
   .querySelector('#data-error')
@@ -15,4 +13,12 @@ export const showAlert = () => {
   setTimeout(() => {
     newAlert.remove();
   }, ALERT_SHOW_TIME);
+};
+
+export const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
 };
